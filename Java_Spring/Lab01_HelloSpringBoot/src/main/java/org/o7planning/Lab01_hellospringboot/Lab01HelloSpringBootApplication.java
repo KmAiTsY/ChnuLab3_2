02f0676fe43c3e0936f582ba012a2ce823ee8108
KmAiTsY <mishagandabura@gmail.com>
2022-02-06 18:34:13 +0200

    Add Java Spring Lab1

---
+++ Java_Spring/Lab01_HelloSpringBoot/src/main/java/org/o7planning/Lab01_hellospringboot/Lab01HelloSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.o7planning.Lab01_hellospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab01HelloSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab01HelloSpringBootApplication.class, args);
+	}
+
+}
